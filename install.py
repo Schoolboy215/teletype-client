b6953983fdcbf304f6b9d5cca6ca129819f5a399
James McKay <james.mckay215@yahoo.com>
2018-10-31 20:28:00 -0400

    Prepped for next Pi test

--- install.py
+++ install.py
@@ -1,1 +1,11 @@
 from crontab import CronTab
+import os
+
+cwd = os.getcwd()
+
+my_cron = CronTab(user='pi')
+job = my_cron.new(command='cd '+cwd+' && python client.py')
+job.minute.every(1)
+
+my_cron.write()
+
