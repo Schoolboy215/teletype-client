4af4ad866362a9258f9ae5791739fb7b4de597b5
James McKay <james.mckay215@yahoo.com>
2018-10-31 20:50:28 -0400

    Made installation prompt the user for urls instead of hard coding

--- install.py
+++ install.py
@@ -3,6 +3,21 @@ import os
 
 cwd = os.getcwd()
 
+SERVER_URL = "https://teletype.personalspaceshow.lawyer"
+REPO_URL = "https://github.com/Schoolboy215/teletype-client"
+
+print("Enter the full address of the server you'd like to connect to.")
+print("(Leave blank to use the default of " + SERVER_URL + ")")
+userInput = input()
+if (userInput)
+    SERVER_URL = userInput
+
+print("Enter the url of the repository to pull updates from")
+print("(Leave blank to use the default of " + REPO_URL + ")")
+userInput = input()
+if (userInput)
+    REPO_URL = userInput
+
 my_cron = CronTab(user='pi')
 job = my_cron.new(command='cd '+cwd+' && python client.py')
 job.minute.every(1)
@@ -10,6 +25,6 @@ job.minute.every(1)
 my_cron.write()
 
 f= open("config.py","w")
-f.write('SERVER_URL = "https://teletype.personalspaceshow.lawyer"\nREPO_URL = "https://github.com/Schoolboy215/teletype-client"')
+f.write('SERVER_URL = "'+SERVER_URL+'"\nREPO_URL = "'+REPO_URL+'"')
 f.close()
 
